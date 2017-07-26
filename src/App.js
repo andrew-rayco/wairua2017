be33dee362cdf59d4dcef9f5723e0b4d309f1b0b
Andrew <andy@andycondon.com>
2017-07-27 11:10:14 +1200

    add raw create-react-app files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>I'd like to welcome you here....</h1>
+        <p>Holy shit I like this. It's pretty zippy.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
