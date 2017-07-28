1ccdcae4f8b0a3dcec558a260783f2a6c64763d8
Andrew <andy@andycondon.com>
2017-07-28 16:56:23 +1200

    initial form styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './normalize.css';
-import './skeleton.css';
-import './index.css';
+import './css/normalize.css';
+import './css/skeleton.css';
+import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
