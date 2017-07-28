c802e161be3745cf3c31fe99bd52816a788f1147
Andrew <andy@andycondon.com>
2017-07-28 17:20:02 +1200

    complete intial form layout

--- src/Form.js
+++ src/Form.js
@@ -82,36 +82,46 @@ export default class Form extends React.Component {
                 <label for="GPphone" id="GPphone-ariaLabel">GP phone</label>
                 <input id="GPphone" name="GPphone" type="text" aria-labelledby="GPphone-ariaLabel"/></div>
             </div>
+          </fieldset>
 
-          </fieldset>
           <fieldset className="fieldset-emergency">
-            <div className="four columns">
-              <label for="Emergencycontact1" id="Emergencycontact1-ariaLabel">Emergency contact 1</label>
-              <input id="Emergencycontact1" name="Emergencycontact1" type="text" aria-labelledby="Emergencycontact1-ariaLabel" className="required" title="Emergency contact 1. This is a required field"/></div>
-            <div className="four columns">
-              <label for="Relationshiptochild" id="Relationshiptochild-ariaLabel">Relationship to child</label>
-              <input id="Relationshiptochild" name="Relationshiptochild" type="text" aria-labelledby="Relationshiptochild-ariaLabel"/></div>
-            <div className="four columns">
-              <label for="Homephone" id="Homephone-ariaLabel">Home phone</label>
-              <input id="Homephone" name="Homephone" type="text" aria-labelledby="Homephone-ariaLabel"/></div>
-            <div className="four columns">
-              <label for="Cellphone" id="Cellphone-ariaLabel">Cell phone</label>
-              <input id="Cellphone" name="Cellphone" type="text" aria-labelledby="Cellphone-ariaLabel" className="required" title="Cell phone. This is a required field"/></div>
-            <div className="four columns">
-              <label for="Emergencycontact2" id="Emergencycontact2-ariaLabel">Emergency contact 2</label>
-              <input id="Emergencycontact2" name="Emergencycontact2" type="text" aria-labelledby="Emergencycontact2-ariaLabel"/></div>
-            <div className="four columns">
-              <label for="Relationshiptochild" id="Relationshiptochild-ariaLabel">Relationship to child</label>
-              <input id="Relationshiptochild" name="Relationshiptochild" type="text" aria-labelledby="Relationshiptochild-ariaLabel"/></div>
-            <div className="four columns">
-              <label for="Homephone" id="Homephone-ariaLabel">Home phone</label>
-              <input id="Homephone" name="Homephone" type="text" aria-labelledby="Homephone-ariaLabel"/></div>
-            <div className="four columns">
-              <label for="Cellphone" id="Cellphone-ariaLabel">Cell phone</label>
-              <input id="Cellphone" name="Cellphone" type="text" aria-labelledby="Cellphone-ariaLabel"/></div>
+            <div className="row">
+              <div className="four columns">
+                <label for="Emergencycontact1" id="Emergencycontact1-ariaLabel">Emergency contact 1</label>
+                <input id="Emergencycontact1" name="Emergencycontact1" type="text" aria-labelledby="Emergencycontact1-ariaLabel" className="required" title="Emergency contact 1. This is a required field"/></div>
+              <div className="four columns">
+                <label for="Relationshiptochild" id="Relationshiptochild-ariaLabel">Relationship to child</label>
+                <input id="Relationshiptochild" name="Relationshiptochild" type="text" aria-labelledby="Relationshiptochild-ariaLabel"/></div>
+              <div className="four columns">
+                <label for="Homephone" id="Homephone-ariaLabel">Home phone</label>
+                <input id="Homephone" name="Homephone" type="tel" aria-labelledby="Homephone-ariaLabel"/></div>
+            </div>
+            <div className="row">
+              <div className="four columns">
+                <label for="Cellphone" id="Cellphone-ariaLabel">Cell phone</label>
+                <input id="Cellphone" name="Cellphone" type="text" aria-labelledby="Cellphone-ariaLabel" className="required" title="Cell phone. This is a required field"/></div>
+              <div className="four columns">
+                <label for="Emergencycontact2" id="Emergencycontact2-ariaLabel">Emergency contact 2</label>
+                <input id="Emergencycontact2" name="Emergencycontact2" type="text" aria-labelledby="Emergencycontact2-ariaLabel"/></div>
+              <div className="four columns">
+                <label for="Relationshiptochild" id="Relationshiptochild-ariaLabel">Relationship to child</label>
+                <input id="Relationshiptochild" name="Relationshiptochild" type="text" aria-labelledby="Relationshiptochild-ariaLabel"/></div>
+            </div>
+            <div className="row">
+              <div className="four columns">
+                <label for="Homephone" id="Homephone-ariaLabel">Home phone</label>
+                <input id="Homephone" name="Homephone" type="text" aria-labelledby="Homephone-ariaLabel"/></div>
+              <div className="four columns">
+                <label for="Cellphone" id="Cellphone-ariaLabel">Cell phone</label>
+                <input id="Cellphone" name="Cellphone" type="text" aria-labelledby="Cellphone-ariaLabel"/></div>
+            </div>
           </fieldset>
-          <div className="four columns">
-            <input type="submit" value="Submit"/></div>
+          <div className="row">
+            <div className="twelve columns">
+              <input type="submit" value="Submit"/>
+            </div>
+          </div>
+
         </form>
 
       </div>
