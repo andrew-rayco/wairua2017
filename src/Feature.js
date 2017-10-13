523af4c55bb0a3ac478ae737c711e1361284596d
Andrew <andy@andycondon.com>
2017-10-13 19:14:53 +1300

    hero section added

--- src/Feature.js
+++ src/Feature.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
+let style = {
+  backgroundImage: 'url(../images/main.jpg)'
+}
+
 const Feature = () => {
   return (
-    <div className="hero">
-      <h2>Mind. Body. Spirit.</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quos velit, facere, deserunt cum distinctio quae sit autem corporis nihil sunt suscipit nostrum repellat fugit minus esse molestiae fugiat ratione.</p>
-    </div>
+    <section className="hero">
+      <div class="background-image" style={style}></div>
+      <div className="hero-content-area">
+        <h1>Mind. Body. Spirit.</h1>
+        <img src="../images/main.jpg" alt=""/>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quos velit, facere, deserunt cum distinctio quae sit autem corporis nihil sunt suscipit nostrum repellat fugit minus esse molestiae fugiat ratione.</h3>
+        <a href="/workshops" class="btn">Learn more</a>
+      </div>
+    </section>
   )
 }
 
