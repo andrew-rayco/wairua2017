40c067faf75674bc3dc0bf759c428b1c8b137b80
Andrew <andy@andycondon.com>
2017-10-17 10:25:14 +1300

    testing successfully

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+describe('<App/> component', () => {
+  const app = shallow(<App />);
+
+  it('renders without crashing', () => {
+    expect(app.length).toEqual(1)
+  });
+
+  it('renders a div with a class of container', () => {
+    expect(app.find('.container').exists()).toBe(true)
+  })
+
+  it('renders a div with a class of App', () => {
+    expect(app.find('.App').exists()).toBe(true)
+  })
+
+})
