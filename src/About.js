f28d7b0264f9a91774568f0844b0cb5be8c51153
Andrew <andy@andycondon.com>
2017-10-17 12:33:18 +1300

    add main icon

--- src/About.js
+++ src/About.js
@@ -3,19 +3,24 @@ import React from 'react'
 const About = () => {
   return (
     <section className="about">
-      
+
       <h2>About Us</h2>
         <div className="about-image">
           <img src="http://placehold.it/400x300" alt="Shane"/>
         </div>
-        <h3>Shane</h3>
-        <p>Laboriosam animi, optio sit quidem. Laboriosam lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla saepe ratione itaque cum amet, recusandae non perspiciatis quasi, beatae soluta, perferendis tempora minima nesciunt quaerat! Laboriosam animi, optio sit quidem.</p>
+        <div className="about-text">
+          <h3>Shane</h3>
+          <p>Laboriosam animi, optio sit quidem. Laboriosam lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla saepe ratione itaque cum amet, recusandae non perspiciatis quasi, beatae soluta, perferendis tempora minima nesciunt quaerat! Laboriosam animi, optio sit quidem.</p>
+        </div>
+
 
         <div className="about-image">
           <img src="http://placehold.it/400x300" alt="Shane"/>
         </div>
-        <h3>Jen</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla saepe ratione itaque cum amet, recusandae non perspiciatis quasi, beatae soluta, perferendis tempora minima nesciunt quaerat! Laboriosam animi, optio sit quidem.</p>
+        <div className="about-text">
+          <h3>Jen</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla saepe ratione itaque cum amet, recusandae non perspiciatis quasi, beatae soluta, perferendis tempora minima nesciunt quaerat! Laboriosam animi, optio sit quidem.</p>
+        </div>
 
     </section>
   )
